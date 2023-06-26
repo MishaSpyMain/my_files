f6af4b7669d92b903d6ecab9ff3396b2aae8cbe6
misha <yoki.makaroki@gmail.com>
2023-06-26 15:00:10 -0400

    2.1 done

---
+++ lesson 2.1 homework.py
@@ -0,0 +1,7 @@
+# make a list of thing and use type() to get data type
+my_list = ["Tony", 2, False, 3.696969]
+
+for item in my_list:
+    print(type(item))
+
+#  item is refering to things inside the list
