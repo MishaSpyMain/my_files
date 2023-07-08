6f08db357638ca6f502576edf3f589c781d6152e
misha <yoki.makaroki@gmail.com>
2023-07-08 18:34:14 -0400

    2.5 done

---
+++ lesson 3.1 homework.py
@@ -0,0 +1,16 @@
+# asking user for 2 numbers then dividing them.
+
+x = input("What is your first number?: ") # requesting first number
+y = input("What is your second number?: ") # requesting second number
+
+
+
+if y != 0:
+    result = int(x) / int(y)
+    print("Answer is:", result)
+else:
+    print("Can't use zeros sorry ;( ")
+
+
+
+
