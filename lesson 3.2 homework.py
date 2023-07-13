12a0b940a36b23d0d7fc01f4eac10ae7aea995ca
misha <yoki.makaroki@gmail.com>
2023-07-13 14:09:38 -0400

    2.5 done

---
+++ lesson 3.2 homework.py
@@ -0,0 +1,14 @@
+
+
+
+
+def print_user_info(first_name, last_name, birth_year, city, email, phone_number):
+    user_info = "Name: {}, Last Name: {}, Birth Year: {}, City: {}, Email: {}, Phone Number: {}".format(
+        first_name, last_name, birth_year, city, email, phone_number
+    )
+    print(user_info)
+
+print_user_info(first_name="Misha", last_name="Non", birth_year="2000", city="Big City", email="idk@gmail.com", phone_number="404040404040")
+
+
+
