299addcf55f0862c00995d118a9febee946c5c39
misha <yoki.makaroki@gmail.com>
2023-07-16 17:01:40 -0400

    2.5 done

---
+++ lesson 1.1 homework.py
@@ -0,0 +1,14 @@
+
+var1 = 25
+var2 = 27.5
+var3 = "i'm not sure"
+
+print("var1 is: ", var1)
+print("var2 is: ", var2)
+print("var3 is: ", var3)
+
+person_number = int(input("What is your number?: "))
+person_word = str(input("What is your word?: "))
+
+print("your number is:", person_number)
+print("Your word is: ", person_word)
